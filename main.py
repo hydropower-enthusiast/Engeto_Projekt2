60ef38f621a3cc01bc8b16d99d0c9a52f1c9ab9c
hydropower-enthusiast <harazinpremysl@gmail.com>
2025-05-13 18:45:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,167 @@
+"""
+projekt_2.py: druhy projekt do Engeto Online Python Akademie
+
+author: Přemysl Harazin
+email: harazinpremysl@gmail.com
+"""
+
+from random import choice
+
+
+
+print("Hi there")
+print("-------------------------------------------------------------------")
+print("""I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.""")
+print("-------------------------------------------------------------------")
+
+def ciselny_generator(k:int)->int:
+    """
+    Funkce generuje k-ciferne cislo (defaultne 4-ciferne), 
+    ktere nezacina 0. Vsechny cifry jsou unikatni.
+    Vraci Integer.
+    
+    :param - int
+    
+    """
+    ciselny_list=['1','2','3','4','5','6','7','8','9']
+    helper_list=[]
+    a=choice(ciselny_list)
+    helper_list.append(a)
+    ciselny_list.pop(ciselny_list.index(a))
+    ciselny_list.insert(0,"0")
+    
+    for xxx in range(0,k-1):
+        b=choice(ciselny_list)
+        helper_list.append(b)
+        ciselny_list.pop(ciselny_list.index(b))
+        
+    c=""
+    for each in helper_list:
+        c=c+each
+        
+    return int(c)
+    
+def kontrola_inputu(uzivatelsky_input2 : str) -> bool:
+    """
+    Funkce kontrolujici uzivateluv input. 
+    Input musi mit spravnou delku, musi to byt cisla,
+    cifry se nesmi opakovat, cislo nesmi zacinat 0, 
+    
+    :param - str
+    
+    """
+    
+    # Kontrola Delky
+    if len(uzivatelsky_input2)!=4:
+        print("Given number does not have the proper length")
+        return False
+    
+    # Kontrola znaku, zda se jedna o cisla
+    for each in uzivatelsky_input2:
+        if each not in ['0','1','2','3','4','5','6','7','8','9']:
+            print("Some signs are not numbers")
+            return False
+        
+    # Kontrola, zda ci­slo nezacina 0
+    if uzivatelsky_input2[0]=="0":
+        print("Number cannot start with 0")
+        return False
+    
+    # Kontrola Duplicitni­ch Hodnot
+    helper_list2=[]
+    for each in uzivatelsky_input2:
+        if each in helper_list2:
+            print("Duplicitni­ hodnoty nejsou dovoleny")
+            return False
+        else:
+            helper_list2.append(each)
+    else:
+        return True
+
+vygenerovane_cislo=ciselny_generator(4)
+vygenerovane_cislo_list=[eeach for eeach in str(vygenerovane_cislo)]
+
+# Cyklus While, ktera bezi tak dlouho, dokud nedojde k uhodnuti spravneho cisla.
+h=True
+pocet_cyklu=0
+while h:
+    pocet_cyklu += 1
+    # Cyklus While not, ktery kontroluje spravnost uzivatelova inputu.
+    zkontrolovana_hodnota=False
+    while not zkontrolovana_hodnota:
+        uzivatelsky_input=input("Enter a number: ")
+        zkontrolovana_hodnota=kontrola_inputu(uzivatelsky_input)
+            
+    if int(uzivatelsky_input)==vygenerovane_cislo:
+        print("Correct, you've guessed the right number on the ",pocet_cyklu," try.")
+        print("-------------------------------------------------------------------")
+        print("That's amazing.")
+        h=False
+        break
+    
+     
+    bull_count=0
+    cow_count=0
+    # Cyklus pocita bulls, (pokud sedi cislo i pozice)
+    # a cows pokud sedi pouze cislo, ale ne pozice. 
+    for z,u in zip(uzivatelsky_input,vygenerovane_cislo_list):
+        if z==u:
+            bull_count += 1
+
+        if z!=u and z in vygenerovane_cislo_list:
+            cow_count += 1
+    if bull_count==1:
+        print(bull_count," Bull")
+    else:
+        print(bull_count," Bulls")
+
+    if cow_count==1:
+        print(cow_count," Cow")
+    else:
+        print(cow_count," Cows")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
